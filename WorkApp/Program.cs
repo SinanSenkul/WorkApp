08766fcefd1c9a3135631f887787e0c2e53a96a1
sinan senkul <sinan@sinan-senkul.local>
2024-07-23 12:13:57 +0300

    if/else case study has been coded

--- WorkApp/Program.cs
+++ WorkApp/Program.cs
@@ -1,23 +1,25 @@
-﻿Console.WriteLine("Lütfen aşağıdaki bilgileri giriniz:");
-Console.Write("T.C. Kimlik Numarası:");
+﻿Console.Write("Lütfen bir sayı giriniz: ");
 // asking user his information down below:
-string personID = Console.ReadLine();
-Console.Write("Adı:");
-string personName = Console.ReadLine();
-Console.Write("Soyadı:");
-string personLastname = Console.ReadLine();
-Console.Write("Telefon Numarası:");
-string personPhoneNum = Console.ReadLine(); //defined it string because we won't need it for any calculation.
-Console.Write("Yaş:");
-int personAge = Convert.ToInt32(Console.ReadLine());
-Console.Write("İlk aldığı ürünün fiyatı:");
-double firstProductPrice = Convert.ToInt32(Console.ReadLine()); //defined it double to allow point numbers
-Console.Write("İkinci aldığı ürünün fiyatı:");
-double secondProductPrice = Convert.ToInt32(Console.ReadLine()); //defined it double  to allow point numbers
-double patikaPoints = (firstProductPrice + secondProductPrice) * 0.1; //defined patikaPoints as a double variable
-Console.WriteLine("------------------------------------------");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 10)
+{
+    Console.WriteLine("Girilen sayı 10'dan büyüktür");
+}
+else if (num == 10)
+{
+    Console.WriteLine("Girilen sayı 10'a eşittir");
+}
+else
+{
+    Console.WriteLine("Girilen sayı 10'dan küçüktür");
+}
 
-//here it writes the information with string interpolation:
-Console.WriteLine($"{personID} Tc numaralı {personName} {personLastname} isimli kişi için kayıt oluşturulmuştur.");
-Console.WriteLine($"{firstProductPrice+secondProductPrice} toplam harcama karşılığı kazanılan %10 patika puan miktarı " +
-    $"-> {patikaPoints}dur."); // last line   
+if (num % 2 == 0)
+{
+    Console.WriteLine("Girilen sayı çifttir");
+}
+
+else
+{
+    Console.WriteLine("Girilen sayı tektir");
+}
