74ebc981b4173b04993509e7b3dffe5adc9083d4
sinan senkul <sinan@sinan-senkul.local>
2024-07-23 11:56:21 +0300

    new commit

--- WorkApp/Program.cs
+++ WorkApp/Program.cs
@@ -7,18 +7,17 @@ string personName = Console.ReadLine();
 Console.Write("Soyadı:");
 string personLastname = Console.ReadLine();
 Console.Write("Telefon Numarası:");
-string personPhoneNum = Console.ReadLine();
+string personPhoneNum = Console.ReadLine(); //defined it string because we won't need it for any calculation.
 Console.Write("Yaş:");
 int personAge = Convert.ToInt32(Console.ReadLine());
 Console.Write("İlk aldığı ürünün fiyatı:");
-int firstProductPrice = Convert.ToInt32(Console.ReadLine());
+double firstProductPrice = Convert.ToInt32(Console.ReadLine()); //defined it double to allow point numbers
 Console.Write("İkinci aldığı ürünün fiyatı:");
-int secondProductPrice = Convert.ToInt32(Console.ReadLine());
+double secondProductPrice = Convert.ToInt32(Console.ReadLine()); //defined it double  to allow point numbers
 double patikaPoints = (firstProductPrice + secondProductPrice) * 0.1; //defined patikaPoints as a double variable
 Console.WriteLine("------------------------------------------");
 
 //here it writes the information with string interpolation:
 Console.WriteLine($"{personID} Tc numaralı {personName} {personLastname} isimli kişi için kayıt oluşturulmuştur.");
 Console.WriteLine($"{firstProductPrice+secondProductPrice} toplam harcama karşılığı kazanılan %10 patika puan miktarı " +
-    $"-> {patikaPoints}dur.");
-
+    $"-> {patikaPoints}dur."); // last line   
